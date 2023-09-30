a5f64c285cc007f1f17d2e7801df128c7c37e432
MOHAMMAD JABIR <79660697+jabir7860@users.noreply.github.com>
2023-09-30 23:38:15 +0530

    Create JAVASCRIPT.js

---
+++ JAVASCRIPT.js
@@ -0,0 +1,28 @@
+// Array of quiz questions
+const questions = [
+  {
+    question: "What is the capital of France?",
+    answers: ["Paris", "London", "Rome", "Berlin"],
+    correctAnswer: "Paris",
+  },
+  {
+    question: "What is the largest ocean in the world?",
+    answers: ["Pacific Ocean", "Atlantic Ocean", "Indian Ocean", "Arctic Ocean"],
+    correctAnswer: "Pacific Ocean",
+  },
+  {
+    question: "What is the tallest mountain in the world?",
+    answers: ["Mount Everest", "K2", "Kangchenjunga", "Lhotse"],
+    correctAnswer: "Mount Everest",
+  },
+  {
+    question: "What is the largest planet in the solar system?",
+    answers: ["Jupiter", "Saturn", " Uranus", "Neptune"],
+    correctAnswer: "Jupiter",
+  },
+  {
+    question: "What is the chemical symbol for water?",
+    answers: ["H2O", CO2, O2, N2"],
+    correctAnswer: "H2O",
+  },
+];
